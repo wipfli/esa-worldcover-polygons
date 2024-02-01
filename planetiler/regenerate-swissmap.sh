f468b5d1cdc7636522202c30a8f05d51cef138de
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-02-01 17:21:22 +0100

    Update planetiler/regenerate-swissmap.sh

--- planetiler/regenerate-swissmap.sh
+++ planetiler/regenerate-swissmap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 ./mvnw clean package --file standalone.pom.xml
-java -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.GloballandcoverMain --maxzoom=11 --output=data/esa-worldcover-polygons.pmtiles
+java -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.GloballandcoverMain --maxzoom=11 --output=data/esa-worldcover-polygons.pmtiles --download
 docker run --rm -it -v "$(pwd)/data":/data -p 8080:8080 maptiler/tileserver-gl -p 8080
 
