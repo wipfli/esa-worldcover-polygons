c184e859668598a7e30e725c90da2e804e724a77
René Müller <48084558+rene-mueller@users.noreply.github.com>
2024-02-01 17:32:48 +0100

    Replace single water layer with bathymetry from natural earth data (#6)
    * added .gitignore
    
    * implemented multiple data sources; added GlaciatedAreasHandler
    
    * added BathymetryHandler [WIP]
    
    * added sorting by depth for bathymetry features
    
    * added more detailed merging of polygons for bathymetry
    
    * moved water and land from EsaHandler to NaturalEarthHandler
    
    * exclude depth 0 in bathymetry
    
    * used min value for MaxZoom in BathymetryHandler
    
    * removed min and max zoom from bathymetry features
    
    * changed minDist and buffer to 0.2 since the merging was not perfect in BathymetryHandler
    
    * adding attribute excludeClasses for excluding globallandcover classes
    
    * Update planetiler/src/main/java/com/onthegomap/planetiler/examples/GloballandcoverProfile.java
    
    ---------
    
    Co-authored-by: Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>

--- planetiler/src/main/java/com/onthegomap/planetiler/examples/GloballandcoverProfile.java
+++ planetiler/src/main/java/com/onthegomap/planetiler/examples/GloballandcoverProfile.java
@@ -2,26 +2,37 @@ package com.onthegomap.planetiler.examples;
 
 import com.onthegomap.planetiler.*;
 import com.onthegomap.planetiler.config.PlanetilerConfig;
+import com.onthegomap.planetiler.examples.handlers.BathymetryHandler;
 import com.onthegomap.planetiler.examples.handlers.EsaHandler;
 import com.onthegomap.planetiler.examples.handlers.NaturalEarthHandler;
 import com.onthegomap.planetiler.stats.Stats;
 import com.onthegomap.planetiler.util.Translations;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GloballandcoverProfile extends ForwardingProfile {
   public static final String ESA_WORLD_COVER_SOURCE = "esa_world_cover";
+  public static final String BATHYMETRY_SOURCE = "natural_earth_bathymetry";
   public static final String NATURAL_EARTH_SOURCE = "natural_earth";
   public static final String LAYER = "globallandcover";
+  public static List<String> includedClasses;
+
 
   public GloballandcoverProfile(Planetiler runner) {
     this(runner.translations(), runner.config(), runner.stats());
   }
 
   public GloballandcoverProfile(Translations translations, PlanetilerConfig config, Stats stats) {
+    List<String> availableClasses = List.of("tree", "shrub", "grass", "crop", "urban", "barren", "ice", "water", "herbaceous", "mangroves", "moss", "land");
+    List<String> excludedClasses = config.arguments().getList("exclude_classes", "Exclude certain classes", List.of());
+    includedClasses = availableClasses.stream().filter(i -> !excludedClasses.contains(i))
+        .collect(Collectors.toList());
+
     List<Handler> layers = List.of(
         new com.onthegomap.planetiler.examples.handlers.EsaHandler(translations, config, stats),
-        new com.onthegomap.planetiler.examples.handlers.NaturalEarthHandler(translations, config, stats)
+        new com.onthegomap.planetiler.examples.handlers.NaturalEarthHandler(translations, config, stats),
+        new com.onthegomap.planetiler.examples.handlers.BathymetryHandler(translations, config, stats)
     );
 
     for (Handler layer : layers) {
@@ -37,6 +48,9 @@ public class GloballandcoverProfile extends ForwardingProfile {
       if (layer instanceof NaturalEarthHandler processor) {
         registerSourceHandler(GloballandcoverProfile.NATURAL_EARTH_SOURCE, processor);
       }
+      if (layer instanceof BathymetryHandler processor) {
+        registerSourceHandler(GloballandcoverProfile.BATHYMETRY_SOURCE, processor);
+      }
     }
   }
 
@@ -47,7 +61,8 @@ public class GloballandcoverProfile extends ForwardingProfile {
 
   @Override
   public String description() {
-    return "Land cover as polygons for the whole world, the following coverages are classified: tree, shrub, grass, crop, urban, barren, ice, water, herbaceous, mangroves, moss, land";
+    return "Land cover as polygons for the whole world, the following coverages are classified: " + String.join(", ", includedClasses) + ". Additionally bathymetry is included.";
+
   }
 
   @Override
