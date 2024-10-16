3f24597562dd705ba5a85c38b318b5c4993c3b5b
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-10-16 19:39:27 +0200

    Antarctica

--- planetiler/src/main/java/com/onthegomap/planetiler/examples/handlers/NaturalEarthHandler.java
+++ planetiler/src/main/java/com/onthegomap/planetiler/examples/handlers/NaturalEarthHandler.java
@@ -7,15 +7,13 @@ import com.onthegomap.planetiler.VectorTile;
 import com.onthegomap.planetiler.config.PlanetilerConfig;
 import com.onthegomap.planetiler.examples.GloballandcoverProfile;
 import com.onthegomap.planetiler.geo.GeometryException;
-import com.onthegomap.planetiler.geo.TileCoord;
 import com.onthegomap.planetiler.reader.SourceFeature;
 import com.onthegomap.planetiler.stats.Stats;
 import com.onthegomap.planetiler.util.Translations;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import org.locationtech.jts.geom.Point;
 
-public class NaturalEarthHandler implements ForwardingProfile.FeatureProcessor, ForwardingProfile.LayerPostProcesser, ForwardingProfile.TilePostProcessor {
+public class NaturalEarthHandler implements ForwardingProfile.FeatureProcessor, ForwardingProfile.LayerPostProcesser {
   private final PlanetilerConfig config;
 
   public NaturalEarthHandler(Translations translations, PlanetilerConfig config, Stats stats) {
@@ -36,7 +34,7 @@ public class NaturalEarthHandler implements ForwardingProfile.FeatureProcessor, 
     NaturalEarthInfo info = switch (sourceFeature.getSourceLayer()) {
       case "ne_10m_ocean" -> new NaturalEarthInfo(minZoom, "water");
       case "ne_10m_land", "ne_10m_minor_islands" -> new NaturalEarthInfo(minZoom, "land");
-      case "ne_10m_glaciated_areas" -> new NaturalEarthInfo(minZoom, "ice");
+      case "ne_10m_glaciated_areas" -> new NaturalEarthInfo(0, "ice");
       default -> null;
     };
 
@@ -48,25 +46,26 @@ public class NaturalEarthHandler implements ForwardingProfile.FeatureProcessor, 
       return;
     }
 
+    if (sourceFeature.getSourceLayer().equals("ne_10m_glaciated_areas")) {
+      try {
+        Point centroid = (Point) sourceFeature.centroid();
+        if (centroid.getY() < 0.7) {
+          return;
+        }
+      } catch (GeometryException e) {
+        System.out.println("Error: " + e);
+      }
+    }
+
     features.polygon(GloballandcoverProfile.LAYER)
         .setMinZoom(info.minZoom)
         .setMaxZoom(config.maxzoom())
-        .setAttr("class", info.clazz);
+        .setAttr("class", info.clazz);    
   }
 
   @Override
   public List<VectorTile.Feature> postProcess(int zoom, List<VectorTile.Feature> items) throws GeometryException {
     return FeatureMerge.mergeNearbyPolygons(items, 1, 1, 0.1, 0.1);
-  }
-
-  @Override
-  public Map<String, List<VectorTile.Feature>> postProcessTile(TileCoord tileCoord,
-    Map<String, List<VectorTile.Feature>> layers) {
-    Map<String, List<VectorTile.Feature>> result = new HashMap<>();
-    for (var key : layers.keySet()) {
-      result.put(key, layers.get(key).stream().skip(1).toList());
-    }
-    return result;
   }
 
   @Override
