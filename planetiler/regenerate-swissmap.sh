6c0915434a7d92fe99eaf83fcecef31a556f0a1b
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-10-17 09:00:35 +0200

    Rename output

--- planetiler/regenerate-swissmap.sh
+++ planetiler/regenerate-swissmap.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 ./mvnw clean package
-java -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.GloballandcoverMain --output-layerstats --maxzoom=9 --output=data/esa-worldcover-polygons.pmtiles --download --force
-
+java -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.GloballandcoverMain --output-layerstats --maxzoom=9 --output=data/esa-worldcover-polygons-v2.pmtiles --download --force
